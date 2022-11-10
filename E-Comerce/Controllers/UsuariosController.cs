17de4a278c91548210c0b1810859c0941ed8f259
DE PAZ MEJIA CRISTIAN YOVANI <2526052021@mail.utec.edu.sv>
2022-11-10 14:09:01 -0600

    898

---
+++ E-Comerce/Controllers/UsuariosController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Comerce.Controllers
+{
+    public class UsuariosController : Controller
+    {
+
+        E_ComerceDBDataContext E_ComerceDB = new E_ComerceDBDataContext();
+
+        // GET: Usuarios
+        public ActionResult Index()
+        {
+
+            List<Usuarios> lista = (from u in E_ComerceDB.Usuarios
+                                            select u).ToList();
+
+            return View(lista);
+        }
+
+        // GET: Usuarios/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Usuarios/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Usuarios/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection, Usuarios datos)
+        {
+            try
+            {
+                datos.Fecha_Inserta = DateTime.Now;
+                datos.Usuario_Inserta = "admin";
+
+                E_ComerceDB.Usuarios.InsertOnSubmit(datos);
+                E_ComerceDB.SubmitChanges();
+
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Usuarios/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Usuarios/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Usuarios/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Usuarios/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
