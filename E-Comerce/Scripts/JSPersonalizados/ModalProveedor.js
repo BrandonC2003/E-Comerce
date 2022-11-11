a76ce8b287b8b387949d101679bf24451381cb60
MARTINEZ VIVAS JUAN ALBERTO <2507292021@mail.utec.edu.sv>
2022-11-11 11:49:09 -0600

    JS ModalProvee

---
+++ E-Comerce/Scripts/JSPersonalizados/ModalProveedor.js
@@ -0,0 +1,16 @@
+﻿$(".btnAgrega").click(function (eve) {
+    $(".form").load("/Proveedor/Create");
+});
+$(".btnUpdate").click(function (eve) {
+    $(".form").load("/Proveedor/Edit/" + $(this).data("id"));
+});
+$(".btnDetail").click(function (eve) {
+    $(".form").load("/Proveedor/Details/" + $(this).data("id"));
+});
+$(".btnDelete").click(function (eve) {
+    $(".form").load("/Proveedor/Delete/" + $(this).data("id"));
+});
+
+
+
+
