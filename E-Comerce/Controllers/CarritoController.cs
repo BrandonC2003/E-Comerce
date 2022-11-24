58c4eb18f1bf460ea4db7b3db13ff24e034e7268
DE PAZ MEJIA CRISTIAN YOVANI <2526052021@mail.utec.edu.sv>
2022-11-23 23:10:27 -0600

    trabajando en carrito

---
+++ E-Comerce/Controllers/CarritoController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Comerce.Controllers
+{
+    public class CarritoController : Controller
+    {
+
+        E_ComerceDBDataContext E_ComerceDB = new E_ComerceDBDataContext();
+
+        // GET: Carrito
+        public ActionResult Index()
+        {
+            List<Productos> list = (from p in E_ComerceDB.Productos select p).ToList();
+
+            return View();
+        }
+
+        // GET: Carrito/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+   
+
+        // GET: Carrito/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Carrito/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
