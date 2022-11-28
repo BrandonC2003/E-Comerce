b4f0855e932c5d0aea97750e53c992a770210d7f
Julio Castro <2508762021@mail.utec.edu.sv>
2022-11-27 21:51:18 -0600

    avance Inicio

---
+++ E-Comerce/Controllers/TiendaController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Comerce.Controllers
+{
+    public class TiendaController : Controller
+    {
+        E_ComerceDBDataContext E_ComerceDB = new E_ComerceDBDataContext();
+
+        // GET: Tienda
+        public ActionResult Index()
+        {
+            IEnumerable<Productos> lista = (from l in E_ComerceDB.Productos
+                                            select l
+                                            
+                                            ).ToList();
+            return View(lista);
+        }
+    }
+}
