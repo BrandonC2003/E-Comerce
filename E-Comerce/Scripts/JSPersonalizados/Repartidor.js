4931535a308f4fa4573079e0c19fae2dd5cdcea8
DE PAZ MEJIA CRISTIAN YOVANI <2526052021@mail.utec.edu.sv>
2022-12-01 22:32:43 -0600

    Diseño de repartidor terminado

---
+++ E-Comerce/Scripts/JSPersonalizados/Repartidor.js
@@ -0,0 +1,12 @@
+﻿$("#btnAgregar").click(function (eve) {
+    $(".modal-body").load("/Repartidor/Create");
+});
+$(".btnEditar").click(function (eve) {
+    $(".modal-body").load("/Repartidor/Edit/" + $(this).data("id"));
+});
+$(".btnDetail").click(function (eve) {
+    $(".modal-body").load("/Repartidor/Details/" + $(this).data("id"));
+});
+$(".btnEliminar").click(function (eve) {
+    $(".modal-body").load("/Repartidor/Delete/" + $(this).data("id"));
+});
