9c15240a7d35b6b54a311dd374e4ec31325ef2c4
Brandon Castro <113487550+BrandonC2003@users.noreply.github.com>
2022-12-07 21:08:07 -0600

    Add files via upload

---
+++ WCFLogin/IService1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFLogin
+{
+    // NOTe: puede usar el comando "Rename" del menú "efactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+
+        [OperationContract]
+        RespuestaUsuario InicioSesion(RespuestaUsuario datos);
+
+        [OperationContract]
+        bool CambiarClave(CambiarClave resp, out string mensaje);
+
+        // TODO: agregue aquí sus operaciones de servicio
+    }
+
+
+    [DataContract]
+    public class RespuestaUsuario
+    {
+        [DataMember]
+        public string Usuario { get; set; }
+
+        [DataMember]
+        public string Contrasena { get; set; }
+
+        [DataMember]
+        public string NombreUsuario { get; set; }
+
+        [DataMember]
+        public int ID_Rol { get; set; }
+
+        [DataMember]
+        public int ID_Usuario { get; set; }
+
+    }
+
+    [DataContract]
+    public class CambiarClave
+    {
+        [DataMember]
+        public int ID_Usuario { get; set; }
+        [DataMember]
+        public string ClaveAct { get; set; }
+        [DataMember]
+        public string ClaveNew { get; set; }
+    }
+        // Utilice un contrato de datos, como se ilustra en el ejemplo siguiente, para agregar tipos compuestos a las operaciones de servicio.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ WCFLogin/Service1.svc.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFLogin
+{
+    //Comentario de prueba del wcf
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Service1 : IService1
+    {
+
+        DBEcomerceDataContext db = new DBEcomerceDataContext();
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public RespuestaUsuario InicioSesion (RespuestaUsuario datos)
+        {
+            var usuario = (from usu in db.Usuarios
+                           where usu.Usuario == datos.Usuario
+                           && usu.Clave == datos.Contrasena
+                           select usu).ToList();
+
+            if (usuario.Count>0)
+            {
+                datos.ID_Usuario = Convert.ToInt32(usuario[0].ID_Usuario);
+                datos.ID_Rol = Convert.ToInt32(usuario[0].ID_Rol);
+                datos.NombreUsuario = usuario[0].Usuario;
+
+            }
+
+            return datos;
+        }
+
+        public bool CambiarClave(CambiarClave resp,out string mensaje)
+        {
+            mensaje = string.Empty;
+            try
+            {
+                sp_CambiarClaveResult result= db.sp_CambiarClave(resp.ID_Usuario,resp.ClaveAct,resp.ClaveNew).Single();
+                db.SubmitChanges();
+                mensaje = result.ToString();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+    }
+}
